4f05934b5e987d6fbbf4ad1a50dc9fac48bbd26b
David Supolík <motivationwithdave@gmail.com>
2024-12-12 16:38:33 +0100

    feat: Add typecasting examples for variable conversion
    Added a Python script demonstrating typecasting, converting variables between data types. The script includes examples of:
    
    - Converting an integer to a string.
    - Converting a float to an integer.
    
    Each conversion is verified by printing the resulting type.

---
+++ lessons/02typecasting.py
@@ -0,0 +1,13 @@
+# TYPECASTING = The process of converting a variable from one data type to another
+#               str(),  int(),  float(),  bool()
+
+name = "Dave Supolík"
+age = 25
+gpa = 3.2
+is_student = True
+
+age = str(age)
+print(type(age))
+
+gpa = int(gpa)
+print(type(gpa))
