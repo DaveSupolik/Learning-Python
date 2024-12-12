7944704d8dad29f10ccae84e65c72ddea80112a5
David Supolík <motivationwithdave@gmail.com>
2024-12-12 16:31:09 +0100

    feat: Demonstrate variables with multiple data types
    Implemented a Python script showcasing the use of variables with different data types. The script demonstrates:
    
    - String variables to store names, food preferences, and email.
    - Integer variables for age, item quantities, and student count.
    - Float variables for price, GPA, and distance.
    - Boolean variables to represent states like student status and availability.
    
    Each data type is accompanied by print statements to display their values interactively.

---
+++ lessons/01variables.py
@@ -0,0 +1,36 @@
+# VARIABLE = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Dave"
+food = "Pizza"
+email = "Dave123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is €{price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+print(f"Are you a student?: {is_student}")
